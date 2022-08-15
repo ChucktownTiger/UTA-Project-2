cc6fa8dfd969ed88b575b279ab05b507bab2ea96
ChucktownTiger <clemsonalum07@gmail.com>
2022-08-14 20:46:45 -0400

    index js edit

---
+++ seeds/index.js
@@ -0,0 +1,21 @@
+const sequelize = require('../config/connection');
+const seedBooks = require('./bookData');
+const seedMovies = require('./movieData');
+const seedShows = require('./showData');
+const seedWebsites = require('./websiteData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedBooks();
+
+  await seedShows();
+
+  await seedWebsites();
+
+  await seedMovies();
+
+  process.exit(0);
+};
+
+seedAll();
