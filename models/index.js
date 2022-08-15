9797922baeef640f15802f30eee6a1cada6f86d8
ChucktownTiger <clemsonalum07@gmail.com>
2022-08-14 20:57:46 -0400

    more model and seed edits

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Book = require('./Book');
+const Movie = require('./Movie');
+const Show = require('./Show');
+const Website = require('./Website');
+
+
+module.exports = { User, Book, Movie, Website, Show };
