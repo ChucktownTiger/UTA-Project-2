dca98234070fb9f91ac921c507dedeb264694c34
ChucktownTiger <clemsonalum07@gmail.com>
2022-08-16 22:48:20 -0400

    removed user data seed editted index created and added place holder images

--- seeds/index.js
+++ seeds/index.js
@@ -3,22 +3,17 @@ const seedBooks = require('./bookData');
 const seedMovies = require('./movieData');
 const seedShows = require('./showData');
 const seedWebsites = require('./websiteData');
-const seedUsers = require('./userData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedBooks();
 
-  await seedUsers();
-
   await seedShows();
 
   await seedWebsites();
 
   await seedMovies();  
-  
-  await seedUsers();
 
   process.exit(0);
 };
