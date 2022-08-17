92747404051d8bb118c59eda0463d954d3e6ba6b
isaias <104476799+mrza1127@users.noreply.github.com>
2022-08-17 17:12:33 -0500

    removed user model and seed

--- seeds/index.js
+++ seeds/index.js
@@ -3,22 +3,17 @@ const seedBooks = require('./bookData');
 const seedMovies = require('./movieData');
 const seedTVShows = require('./showData');
 const seedWebsites = require('./websiteData');
-const seedUsers = require('./userData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedBooks();
 
-  await seedUsers();
-
   await seedTVShows();
 
   await seedWebsites();
 
   await seedMovies();  
-  
-  await seedUsers();
 
   process.exit(0);
 };
