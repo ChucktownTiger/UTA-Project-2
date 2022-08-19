b7855f5a25d0b20771939925d1f0f60b3b7e5794
ChucktownTiger <clemsonalum07@gmail.com>
2022-08-18 23:00:56 -0400

    heroku please work

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,28 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = mysql.createConnection({
-  host: "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "abneomai0ags3zeb",
-  password: "b4yxh9q7tsmugklo",
-  database: "kgbw6d4abn4gb1zg"
-})
+
+
+  const sequelize = new Sequelize('kgbw6d4abn4gb1zg', 'abneomai0ags3zeb', 'b4yxh9q7tsmugklo', {
+    host: 'cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect:'mysql' 
+  });
+
+// const sequelize = new Sequelize(
+//   {
+//     host: "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user: "abneomai0ags3zeb",
+//     password: "b4yxh9q7tsmugklo",
+//     database: "kgbw6d4abn4gb1zg"
+//   }
+// );
+
+// const sess = sequelize.createConnection({
+//   host: "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   user: "abneomai0ags3zeb",
+//   password: "b4yxh9q7tsmugklo",
+//   database: "kgbw6d4abn4gb1zg"
+// })
 
 module.exports = sequelize;
