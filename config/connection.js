2a3b2cd345a14b91d1266304d321a51bfde0687c
ChucktownTiger <clemsonalum07@gmail.com>
2022-08-18 23:03:11 -0400

    heroku deployed

--- config/connection.js
+++ config/connection.js
@@ -9,20 +9,5 @@ require('dotenv').config();
     dialect:'mysql' 
   });
 
-// const sequelize = new Sequelize(
-//   {
-//     host: "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//     user: "abneomai0ags3zeb",
-//     password: "b4yxh9q7tsmugklo",
-//     database: "kgbw6d4abn4gb1zg"
-//   }
-// );
-
-// const sess = sequelize.createConnection({
-//   host: "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   user: "abneomai0ags3zeb",
-//   password: "b4yxh9q7tsmugklo",
-//   database: "kgbw6d4abn4gb1zg"
-// })
 
 module.exports = sequelize;
